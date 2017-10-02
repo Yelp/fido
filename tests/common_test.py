70b624d5b9cbc14cb811a9676b7bf6ad3da3724b
Francesco Agosti <fra@yelp.com>
2017-10-02 11:26:41 -0700

    fix: update listify_headers to also convert header keys to byte strings

--- tests/common_test.py
+++ tests/common_test.py
@@ -6,7 +6,7 @@ from fido.common import listify_headers
 
 def test_list_value_not_listified():
     header = {'foo': ['bla']}
-    new_header = {'foo': [b'bla']}
+    new_header = {b'foo': [b'bla']}
     with mock.patch('fido.common.Headers') as mock_header:
         listify_headers(header)
     mock_header.assert_called_once_with(new_header)
@@ -14,7 +14,15 @@ def test_list_value_not_listified():
 
 def test_header_value_listified():
     header = {'foo': 'bla'}
-    new_header = {'foo': [b'bla']}
+    new_header = {b'foo': [b'bla']}
+    with mock.patch('fido.common.Headers') as mock_header:
+        listify_headers(header)
+    mock_header.assert_called_once_with(new_header)
+
+
+def test_byte_header_stays_byte():
+    header = {b'foo': [b'bla']}
+    new_header = {b'foo': [b'bla']}
     with mock.patch('fido.common.Headers') as mock_header:
         listify_headers(header)
     mock_header.assert_called_once_with(new_header)
